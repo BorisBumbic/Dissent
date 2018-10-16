15b421a60e3b8ba94f2e63743f65092e9e46c340
Ramy Niranjan <ramy.niranjan@gmail.com>
2018-10-16 14:23:00 +0200

    Connceting Twitter

---
+++ DissentApp/Dissent/Controllers/SecondHomeController.cs
@@ -0,0 +1,45 @@
+﻿using Dissent.Credentials;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace Dissent.Controllers
+{
+    public class SecondHomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TwitterAuth()
+        {
+            var appCreds = new ConsumerCredentials(MyCredentials.CONSUMER_KEY, MyCredentials.CONSUMER_SECRET);
+            var redirectURL = "http://" + Request.Host.Value + "/Home/ValidateTwitterAuth";
+            var authenticationContext = AuthFlow.InitAuthentication(appCreds, redirectURL);
+
+            return new RedirectResult(authenticationContext.AuthorizationURL);
+        }
+
+        public ActionResult ValidateTwitterAuth()
+        {
+            if (Request.Query.ContainsKey("oauth_verifier") && Request.Query.ContainsKey("authorization_id"))
+            {
+                var verifierCode = Request.Query["oauth_verifier"];
+                var authorizationId = Request.Query["authorization_id"];
+
+                var userCreds = AuthFlow.CreateCredentialsFromVerifierCode(verifierCode, authorizationId);
+                var user = Tweetinvi.User.GetAuthenticatedUser(userCreds);
+
+                ViewBag.User = user;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ DissentApp/Dissent/Controllers/TweetController.cs
@@ -0,0 +1,41 @@
+﻿using Dissent.Credentials;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace Dissent.Controllers
+{
+
+    public class TweetController : Controller
+    {
+        private readonly ITwitterCredentials _credentials;
+
+        public TweetController()
+        {
+            //Auth.SetUserCredentials"CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+
+
+            _credentials = MyCredentials.GenerateCredentials();
+
+            Auth.SetCredentials(_credentials);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TwitterResult(string input)
+        {
+            var matchingTweets = Search.SearchTweets(input);
+            return View(matchingTweets);
+        }
+    }
+}

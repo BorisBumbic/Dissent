e0eaa222d80a845bde0e3950973e332cc83e8a93
Boris Bumbic <boris.bumbic@gmail.com>
2018-10-17 15:39:50 +0200

    k

--- DissentApp/Dissent/Controllers/TweetController.cs
+++ DissentApp/Dissent/Controllers/TweetController.cs
@@ -1,4 +1,5 @@
 ﻿using Dissent.Credentials;
+using Dissent.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace Dissent.Controllers
 {
@@ -29,13 +31,38 @@ namespace Dissent.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult TwitterResult(string input)
-        {
-            var matchingTweets = Search.SearchTweets(input).First();
+        //[HttpPost]
+        //public ActionResult TwitterResult(string input)
+        //{
+        //    var matchingTweets = Search.SearchTweets(input).First();
+        //    //Tweet.
 
-            return Ok(matchingTweets);
-            //return View(matchingTweets);
+        //    return Ok(matchingTweets);
+        //    //return View(matchingTweets);
+        //}
+
+        public List<Tweets> TwitterResult(string input)
+        {
+            var searchParameter = new SearchTweetsParameters(input)
+            {
+                GeoCode = new GeoCode(59.3289, 18.0649, 15, DistanceMeasure.Kilometers)
+            };
+            List<ITweet> matchingTweets = Search.SearchTweets(searchParameter).ToList();
+            List<Tweets> tweetList = new List<Tweets>();
+            foreach (var item in matchingTweets)
+            {
+                tweetList.Add(new Tweets
+                {
+                    id = item.IdStr,
+                    text = item.FullText,
+                    language = item.Language.ToString(),
+                    
+
+                });
+                
+                //Services.SentimentApiServices.RequestSentiment(tweetList);
+            }
+            return tweetList;
         }
     }
 }
--- DissentApp/Dissent/Services/SentimentApiService.cs
+++ DissentApp/Dissent/Services/SentimentApiService.cs
@@ -1,59 +1,40 @@
-﻿//using Newtonsoft.Json;
-//using System;
-//using System.Net.Http;
-//using System.Net.Http.Headers;
-//using System.Text;
-//using System.Web;
+﻿using Dissent.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
 
-//namespace Dissent.Services
-//{
-//    public class SentimentApiService
-//    {
-//        static async void MakeRequest(string input)
-//        {
-//            var client = new HttpClient();
-//            var queryString = HttpUtility.ParseQueryString(string.Empty);
+namespace Dissent.Services
+{
+    public class SentimentApiService
+    {
+        public static async void RequestSentiment(List<Tweets> input)
+        {
+            var client = new HttpClient();
+            //var queryString = HttpUtility.ParseQueryString("");
 
-//            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "6712d8ff97cc46df9fff72c086d93709");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "6712d8ff97cc46df9fff72c086d93709");
 
-//            var uri = "https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + queryString;
+            var uri = "https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" /*+ queryString*/;
 
-//            HttpResponseMessage response;
+            HttpResponseMessage response;
 
+            string inp = JsonConvert.SerializeObject(input);
 
-//            //var r = new RootObject
-//            //{
-//            //    documents = new List<Document>
-//            //    {
-//            //       new Document
-//            //       {
-//            //           text=input,
-//            //           language="en",
-//            //           id="1"
-//            //       },
-//            //        new Document
-//            //       {
-//            //           text="Trump bla bla",
-//            //           language="en",
-//            //           id="2"
-//            //       },
+            byte[] byteData = Encoding.UTF8.GetBytes(inp);
 
-//            //    }
-//            //};
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
 
-//            string inp = JsonConvert.SerializeObject(r);  
+                var result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+            }
 
-//            byte[] byteData = Encoding.UTF8.GetBytes(inp);
-
-//            using (var content = new ByteArrayContent(byteData))
-//            {
-//                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-//                response = await client.PostAsync(uri, content);
-
-//                var result = await response.Content.ReadAsStringAsync();
-//                Console.WriteLine(result);
-//            }
-
-//        }
-//    }
-//}
+        }
+    }
+}

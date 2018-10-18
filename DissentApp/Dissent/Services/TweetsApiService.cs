9ef501008de3249e5c7af8b4836dd70550d9c8ed
Boris Bumbic <boris.bumbic@gmail.com>
2018-10-18 14:08:25 +0200

    g

--- DissentApp/Dissent/Services/TweetsApiService.cs
+++ DissentApp/Dissent/Services/TweetsApiService.cs
@@ -1,11 +1,6 @@
 ﻿using Dissent.Models;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using Tweetinvi;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;

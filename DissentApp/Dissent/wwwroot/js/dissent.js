8e2c3258d8b927f185b3d05358717dac1c23a122
Boris Bumbic <boris.bumbic@gmail.com>
2018-10-22 18:19:25 +0200

    d

--- DissentApp/Dissent/wwwroot/js/dissent.js
+++ DissentApp/Dissent/wwwroot/js/dissent.js
@@ -1,47 +1,11 @@
-﻿
-
- 
-
-//async function showTweetSentiment() {
-//    let userinput = "";
-//    byId("userInput").value= userinput
-//    let response = await fetch("/Tweet/TwitterResult/?input=" + userinput, {
-//        method: "get"
-//    });
-//    let html
-//    let sentimentResponse = await response.json();
-//    for (let result of sentimentResponse) {
-
-//        html +=
-//            `<tr>
-               
-//                <td>${ result.text}</td>
-//                <td>${ result.language}</td>
-//                <td>${ result.sentiment}</td>
-              
-//            </tr>`
-//    }
-
-//    byId("sentimentResult").innerHTML = html;
-
-
-//}  
-
-//function byId(id) {
-//    return document.getElementById(id);
-//}
-function initMap() {
-    var app = new Vue({
+﻿var app = new Vue({
         el: "#app",
         data: {
            
             map: "",
-            mapOptions: {},
-            marker:"",
             sentimentResponse: [],
             userinput: "",
-            lat: "",
-            lng: "",
+            latLng: [lat = 59.40316, lng = 17.94479]
         },
         async created() {
 
@@ -53,13 +17,13 @@ function initMap() {
             initMap: function () {
                 map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 8,
-                    center: new google.maps.LatLng(59.40316, 17.94479),
+                    center: new google.maps.LatLng(this.latLng),
                     mapTypeId: 'hybrid'
                 });
 
                 this.marker = new google.maps.Marker({
                    
-                    position: { lat: 59.40316, lng: 17.94479 },
+                    position: {this.latLng},
                     map: map,
                     title: 'Drop me at the desired location!',
                     draggable: true
@@ -85,29 +49,8 @@ function initMap() {
             }
         }
     });
-}
+
 
 
     
-
-//new Vue({
-//    el: "#map",
-//    data: {
-//        sentimentResponse: [],
-//        userinput: "",
-     
-//    },
-    
-//    methods: {
-
-//        async showTweetSentiment() {
-//            let response = await fetch("/Tweet/TwitterResult/?input="+this.userinput,{
-//                method: "get"
-//            });
-
-//            this.sentimentResponse = await response.json();
-//            console.log("sentimentResponse", this.sentimentResponse);
-//        }
-//    }
-//});
 

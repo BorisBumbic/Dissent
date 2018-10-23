52938b0d0a85dd06626a373fd0a92ece1109491c
AWA5CG8126BTQ\Administrator <petergo2school@gmail.com>
2018-10-23 10:22:47 +0200

    atabase

--- DissentApp/Dissent/Controllers/TweetController.cs
+++ DissentApp/Dissent/Controllers/TweetController.cs
@@ -30,21 +30,17 @@ namespace Dissent.Controllers
         {
             return View();
         }
-<<<<<<< HEAD
-  
-        [HttpPost]
-        public async Task<ActionResult> TwitterResult(string input)
-        {
-            List<ITweet> incomingTweets = TweetsApiService.GetTweets(input);
-=======
 
-        // xxxxxxxx?input=qqqq&lat=qqqq&lng=qqqq&radius=qqqqqqq
+  
+        //[HttpPost]
+        //public async Task<ActionResult> TwitterResult(string input)
+        //{
+        //    List<ITweet> incomingTweets = TweetsApiService.GetTweets(input);
 
         [HttpGet]
         public async Task<ActionResult> TwitterResult(string input, double lat, double lng, int radius)
         {
             List<ITweet> incomingTweets = TweetsApiService.GetTweets(input, lat, lng, radius);
->>>>>>> 5470e61ea1408d9efb6ccac3507fb2a086e49d88
 
             List<Tweets> tweetsMiddleList = TweetsApiService.TweetsToTweetsModelList(incomingTweets);
 
@@ -59,12 +55,9 @@ namespace Dissent.Controllers
 
             await SentimentApiService.RequestSentiment(tweetsMiddleList, tweetsFinalList);
 
-<<<<<<< HEAD
 
             return Ok (tweetsFinalList);
-=======
             return Ok(tweetsFinalList);
->>>>>>> 5470e61ea1408d9efb6ccac3507fb2a086e49d88
         }
     }
 }
